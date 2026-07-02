7191e45695246d23cd36835e13cbbfbc90c40409
ngocson2g <ngocson1265@gmail.com>
2026-07-02 16:41:24 +0700

    upadte docshare v3, auto remove cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'docshare-v2';
+const CACHE_NAME = 'docshare-v3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -10,6 +10,18 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
